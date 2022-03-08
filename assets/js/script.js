8ade01997cc0a6bef87998e3a2ca1fd95bc52afa
RevChrisWarren <chriswarren@Chriss-Mac-mini.attlocal.net>
2022-03-08 11:06:25 -0600

    working on displaying questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,109 +2,96 @@ var highScoreEl = document.getElementById("highscore");
 var timerEl = document.getElementById("timer-count");
 var mainEl = document.getElementById("main");
 var btn = document.querySelector("button");
+var instructEl = document.getElementById("instructions");
+var ansBtn1 = document.getElementById("answerbtn1");
+var ansBtn2 = document.getElementById("answerbtn2");
+var ansBtn3 = document.getElementById("answerbtn3");
+var ansBtn4 = document.getElementById("answerbtn4");
+
 
 var javascriptQuestions = [
   {
       question: "Who invented Javascript?",
-      answers: {
+      
           a: "Steve Jobs",
           b: "Bill Gates",
           c: "Brendan Eich",
-          d: "Linus Torvalds"
-      },
+          d: "Linus Torvalds",
+      
       correctAnswer: "c"
   },  
   {
     question: "Which of these is NOT a data type in Javascript?",
-    answers: {
         a: "string",
         b: "text",
         c: "number",
-        d: "object"
-    },
+        d: "object",
     correctAnswer: "b"
   },
   {
       question: "What do you use to set off items in an array?",
-      answers: {
           a: "square brackets",
           b: "curly brackets",
           c: "parentheses",
-          d: "semi-colons"
-      },
+          d: "semi-colons",
       correctAnswer: "a"
   },
   {
       question: "Given Math.ceil(4.1), what will be the result?",
-      answers: {
           a: "4.1",
           b: "4",
           c: "0",
-          d: "5"
-  },
+          d: "5",
   correctAnswer: "d"
 },
   {
         question: "What is the purpose of a switch statement?",
-        answers: {
             a: "to turn an animation on or off",
             b: "to perform different actions based on different conditions",
             c: "to change the default setting of localStorage",
-            d: "to return a text string in lowercase"
-        },
+            d: "to return a text string in lowercase",
         correctAnswer: "b"
   },
   {
       question: "What is 'hoisting' in Javascript?",
-      answers: {
           a: "Javascript's default behavior of moving declarations to the top",
           b: "Javascript's behaior of compatibility with older web browsers",
           c: "Javascript's ability to customize animations",
-          d: "Javascript's way of lifting errors for rapid debugging"
-      },
+          d: "Javascript's way of lifting errors for rapid debugging",
       correctAnswerr : "a"
   },
   
   {
       question: "To what does 'this' refer when it is used alone?", 
-      answers: {
         a: "the global object",
         b: "the most recently named variable",
         c: "the most recently called function",
-        d: "the object directly following 'this'"
-      },
+        d: "the object directly following 'this'",
       correctAnswer: "a"
   },
 
   {
   question: "What is the purpose of a 'while' loop?",
-    answers: {
         a: "to create a countdown while waiting for a response",
         b: "to write information to sessionStorage",
         c: "to invoke a callback of a previously used function",
-        d: "to loop through a block of code as long as a specified condition exists"
-    },
+        d: "to loop through a block of code as long as a specified condition exists",
     correctAnswer: "d"
 },
 {
 question: "What is used to set off the code to executed in a function?", 
-    answers: {
         a: "parentheses",
         b: "commas",
         c: "curly brackets",
-        d: "square brackets"
-    },
+        d: "square brackets",
     correctAnswer: "c"
 },
 {
 question: "What word is NOT reserved in Javascript?",
-    answers: {
         a: "true",
         b: "break",
         c: "car",
-        d: "debugger"
-
-    },
+        d: "debugger",
     correctAnswer: "c"
 },
 
@@ -121,9 +108,12 @@ startBtnEl.innerHTML = "Click to Begin"
 button.appendChild(startBtnEl);
 
 mainEl.textContent = "Javascript Code Quiz!"
+instructEl.textContent = "When you begin the quiz, you will have two minutes to answer ten questions about Javascript. Your score will equal the time remaining. If you submit an incorrect response, ten seconds will be deducted from the time remaining. Afterwards you can check your score against the high score. Have fun!"
 
 
 function quiz () {
+    instructEl.textContent = "";
+    removeStartBtn();
     var timeLeft = 120;
 var timeInterval = setInterval(function() {
         if (timeLeft > 1) {
@@ -137,12 +127,41 @@ var timeInterval = setInterval(function() {
         clearInterval(timeInterval);
         displayMessage();
     }
+    
 }, 1000);
+
+function removeStartBtn() {
+    var elem = document.getElementById('button');
+    elem.parentNode.removeChild(elem);
+    return false;
+}
 
 }
 function buildQuiz() {
+
     for (var i = 0; i < javascriptQuestions.length; i++) {
     mainEl.innerHTML = javascriptQuestions[i].question;
+
+    
+    
+    var ansBtn1 = document.createElement("button");
+    var ansBtn2 = document.createElement("button");
+    var ansBtn3 = document.createElement("button");
+    var ansBtn4 = document.createElement("button");
+
+    for (var i = 0; i < javascriptQuestions.length; i++)
+    ansBtn1.textContent = javascriptQuestions[i].a;
+    for (var i = 0; i < javascriptQuestions.length; i++)
+    ansBtn2.textContent = javascriptQuestions[i].b;
+    for (var i = 0; i < javascriptQuestions.length; i++)
+    ansBtn3.textContent = javascriptQuestions[i].c;
+    for (var i = 0; i < javascriptQuestions.length; i++)
+    ansBtn4.textContent = javascriptQuestions[i].d;
+
+    answerbtn1.appendChild(ansBtn1);
+    answerbtn2.appendChild(ansBtn2);
+    answerbtn3.appendChild(ansBtn3);
+    answerbtn4.appendChild(ansBtn4);
 }
 }
 
